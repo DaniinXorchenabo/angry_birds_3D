eb6094c03e692d89685f9719e7dc5a35411866aa
DaniinXorchenabo <rkbcu@mail.ru>
2020-10-28 11:37:45 +0300

    upgrade rotate machine

--- NowatorWeb_3_0_2020/Assets/scripts/birds.cs
+++ NowatorWeb_3_0_2020/Assets/scripts/birds.cs
@@ -15,6 +15,17 @@ public class birds : MonoBehaviour
     // private Vector3 dlobal_radius = Vector3.zero;
     private float globalRotationRight = 0.0f;
     private float globalRotationUp = 0.0f;
+    private Vector3 spase_progress;
+
+    // private float radians_setup_right = 0.2f;
+    private static float max_radians_right = 10.0f;
+    private float right_radians_progress;
+
+
+    // private float radians_setup_up = 0.2f;
+    private static float max_radians_up = 50.0f;
+    private float up_radians_progress;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +33,15 @@ public class birds : MonoBehaviour
         my_Trans = transform;
         _rb = GetComponent<Rigidbody>();
         _rb.isKinematic = true;
-        maxDistanse = centerRotatePoint.localPosition - maxBackPoint.localPosition;
+
+        maxDistanse = maxBackPoint.localPosition - centerRotatePoint.localPosition;
+        spase_progress = maxDistanse;
+
+        right_radians_progress = max_radians_right;
+        // radians_setup_right = right_radians_progress * 0.05f;
+
+        up_radians_progress = max_radians_up;
+
     }
 
     // Update is called once per frame
@@ -34,7 +53,8 @@ public class birds : MonoBehaviour
     void FixedUpdate(){
         //_rb.AddForce(Vector3.up * 1 * Input.GetAxis("stretching"), ForceMode.Impulse);
         if (Input.GetAxis("stretching") != 0){
-            Vector3 radius = Vector3.right * (float)0.05f;
+            Vector3 radius = spase_progress * (float)0.01f;
+            spase_progress = spase_progress - radius;
             my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, -globalRotationUp);
             my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, -globalRotationRight);
             my_Trans.localPosition = my_Trans.localPosition + radius;
@@ -42,13 +62,35 @@ public class birds : MonoBehaviour
             my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, globalRotationUp);
 
         }
-        float rotationRight = Input.GetAxis("Horizontal") * 0.2f;
-        globalRotationRight += rotationRight;
-        my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, rotationRight);
+        if (Input.GetAxis("Horizontal") != 0){
+            float rotationRight = Input.GetAxis("Horizontal") * (Mathf.Abs(right_radians_progress) * 0.05f);
+            if (Mathf.Abs(rotationRight) > 0.01f || rotationRight * right_radians_progress < 0.0f){
+                globalRotationRight += rotationRight;
+                right_radians_progress = right_radians_progress - rotationRight;
+                if (Mathf.Abs(right_radians_progress) > max_radians_right){
+                    right_radians_progress = -right_radians_progress;
+                }
+                // radians_setup_right = Mathf.Abs(right_radians_progress) * 0.05f;
+                my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, rotationRight);
+            }
+        }
 
-        float rotationUp = Input.GetAxis("Vertical") * 0.2f;
-        globalRotationUp += rotationUp;
-        my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, rotationUp);
+        // print(globalRotationRight);
+
+        if (Input.GetAxis("Vertical") != 0){
+            float rotationUp = Input.GetAxis("Vertical") * (Mathf.Abs(up_radians_progress) * 0.01f);
+            if (Mathf.Abs(rotationUp) > 0.01f || rotationUp * up_radians_progress < 0.0f){
+
+                globalRotationUp += rotationUp;
+                up_radians_progress = up_radians_progress - rotationUp;
+                if (Mathf.Abs(up_radians_progress) > max_radians_up){
+                    up_radians_progress = -up_radians_progress;
+                }
+                // radians_setup_right = radians_setup_right - 
+                my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, rotationUp);
+                // print(globalRotationUp);
+            }
+        }
     }
 
 }
