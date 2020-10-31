19aea7faca8e5de5afd570937cc436acb12c3308
DaniinXorchenabo <rkbcu@mail.ru>
2020-10-31 18:16:30 +0300

    add forse in bird

---
+++ NowatorWeb_3_0_2020/Assets/scripts/BeforeFlyPreparation.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class BeforeFlyPreparation : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public Transform centerRotatePoint;
+    public Transform maxBackPoint;
+    public Transform my_Trans;
+    private Vector3 maxDistanse;
+    // private Vector3 dlobal_radius = Vector3.zero;
+    private float globalRotationRight = 0.0f;
+    private float globalRotationUp = 0.0f;
+    private Vector3 spase_progress;
+
+    // private float radians_setup_right = 0.2f;
+    private static float max_radians_right = 10.0f;
+    private float right_radians_progress;
+
+
+    // private float radians_setup_up = 0.2f;
+    private static float max_radians_up = 50.0f;
+    private float up_radians_progress;
+
+    private bool spase_on_click = false;
+
+    private FlyingScript flyingScript;
+
+    // Start is called before the first frame update
+
+    void Awake(){
+        my_Trans = transform;
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+
+        maxDistanse = maxBackPoint.localPosition - centerRotatePoint.localPosition;
+
+        flyingScript = gameObject.GetComponent<FlyingScript>();
+        flyingScript.enabled = false;
+        flyingScript.init_param(my_Trans, _rb, centerRotatePoint.localPosition, maxDistanse);
+    }
+    void Start()
+    {
+        spase_progress = maxDistanse;
+        right_radians_progress = max_radians_right;
+        up_radians_progress = max_radians_up;
+
+    }
+
+    void FixedUpdate(){
+        //_rb.AddForce(Vector3.up * 1 * Input.GetAxis("stretching"), ForceMode.Impulse);
+        if (Input.GetAxis("stretching") != 0){
+            if (!spase_on_click){
+                spase_on_click = true;
+            }
+            Vector3 radius = spase_progress * (float)0.01f;
+            spase_progress = spase_progress - radius;
+            my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, -globalRotationUp);
+            my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, -globalRotationRight);
+            my_Trans.localPosition = my_Trans.localPosition + radius;
+            my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, globalRotationRight);
+            my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, globalRotationUp);
+
+        } else {
+            if (spase_on_click){  // кнопка была нажата, а теперь отпущена
+                flyingScript.enabled = true;
+                this.enabled = false;
+            }
+        }
+        if (Input.GetAxis("Horizontal") != 0){
+            float rotationRight = Input.GetAxis("Horizontal") * (Mathf.Abs(right_radians_progress) * 0.05f);
+            if (Mathf.Abs(rotationRight) > 0.01f || rotationRight * right_radians_progress < 0.0f){
+                globalRotationRight += rotationRight;
+                right_radians_progress = right_radians_progress - rotationRight;
+                if (Mathf.Abs(right_radians_progress) > max_radians_right){
+                    right_radians_progress = -right_radians_progress;
+                }
+                // radians_setup_right = Mathf.Abs(right_radians_progress) * 0.05f;
+                my_Trans.RotateAround(centerRotatePoint.position, Vector3.up, rotationRight);
+            }
+        }
+
+        // print(globalRotationRight);
+
+        if (Input.GetAxis("Vertical") != 0){
+            float rotationUp = Input.GetAxis("Vertical") * (Mathf.Abs(up_radians_progress) * 0.01f);
+            if (Mathf.Abs(rotationUp) > 0.01f || rotationUp * up_radians_progress < 0.0f){
+
+                globalRotationUp += rotationUp;
+                up_radians_progress = up_radians_progress - rotationUp;
+                if (Mathf.Abs(up_radians_progress) > max_radians_up){
+                    up_radians_progress = -up_radians_progress;
+                }
+                // radians_setup_right = radians_setup_right - 
+                my_Trans.RotateAround(centerRotatePoint.position, Vector3.forward, rotationUp);
+                // print(globalRotationUp);
+            }
+        }
+    }
+
+}
---
+++ NowatorWeb_3_0_2020/Assets/scripts/FlyingScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingScript : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Transform my_tr;
+    private Vector3 start_center_point;
+    private Vector3 flying_direction;
+    private float powerfull = 0.0f;
+    private int counter = 0;
+    private float script_start_time;
+
+
+    public void init_param(Transform my_trans,
+                    Rigidbody my_rb,
+                    Vector3 center_p,
+                    Vector3 distanse_v){
+        my_tr = my_trans;
+        _rb = my_rb;
+        start_center_point = center_p;
+        flying_direction = distanse_v;
+    }
+    // Start is called before the first frame update
+    void OnEnable(){
+        _rb.isKinematic = false;
+    }
+    void Start()
+    {
+        
+    }
+    void FixedUpdate(){
+        PowerControl();
+    }
+
+    void PowerControl(){
+        if (counter == 0){
+            script_start_time = (float)Time.realtimeSinceStartup;
+        }
+        if ((start_center_point - my_tr.localPosition).z >= 0.0f){
+            _rb.AddRelativeForce(new Vector3(0.0f, 0.0f, generateForse()));
+        }
+        counter++;
+
+    }
+     float generateForse(){
+        float time = (float)Time.realtimeSinceStartup - script_start_time;
+        return 30.0f;
+     }
+
+
+}
